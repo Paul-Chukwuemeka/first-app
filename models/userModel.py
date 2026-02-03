9519ab4f5a0638f4066ee9e3e23914ddaac55402
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2026-02-03 15:13:14 +0100

    Add user token management and user retrieval endpoints
    - Introduced token versioning in the User model.
    - Implemented OAuth2 password bearer for token handling.
    - Added user retrieval functionality by ID and current user endpoint.
    - Created new user routes file for future user-related operations.

--- models/userModel.py
+++ models/userModel.py
@@ -1,6 +1,8 @@
 from utils.db import Base
 from sqlalchemy import Column,Integer,String,DateTime,func
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped,mapped_column
+
 import uuid
 
 class User(Base):
@@ -9,6 +11,7 @@ class User(Base):
     username = Column(String,nullable=False,unique=True)
     email = Column(String,nullable=False,unique=True)
     password = Column(String, nullable=False)
+    token_version: Mapped[int] =  mapped_column(default=0)
     created_at = Column(DateTime(timezone=True),server_default=func.now())
     updated_at = Column(DateTime(timezone=True),onupdate=func.now())
     
--- routes/auth.py
+++ routes/auth.py
@@ -4,10 +4,21 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from pydantic import BaseModel
 from typing import Optional
+from uuid import UUID
 from utils.crypto import verify, hash_password
 from utils.generate_jwt import create_token
-
+from fastapi.security import OAuth2PasswordBearer
+from dotenv import load_dotenv
 from models.userModel import User
+import os
+from jose import jwt
+
+
+
+load_dotenv()
+
+secret = os.getenv("SECRET") 
+algorithm = "HS256"
 
 class NewUser(BaseModel):
     username: str
@@ -20,6 +31,20 @@ class loginUser(BaseModel):
 
 
 router = APIRouter()
+
+oauth = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def getUser(token = Depends(oauth), db: Session = Depends(get_db)):
+    value = jwt.decode(token,str(secret),algorithms=[algorithm])
+    id = value["id"]
+    found = get_user_by_id(id, db)
+    if not found or isinstance(found, Exception):
+        raise HTTPException(status_code=400,detail="invalid token")
+    if (found.token_version != value["token_version"]):
+        raise HTTPException(status_code=400,detail="invalid token")
+    return found
+
 
 @router.post("/users/add")
 def create_new_user(user: NewUser,db: Session = Depends(get_db)):
@@ -52,9 +77,10 @@ def login(user: loginUser,db: Session = Depends(get_db)):
         checkPassword = verify(user.password, str(found.password))
         if not checkPassword:
             raise HTTPException(status_code=400,detail="incorrect password")
+        found.token_version = found.token_version + 1
+        db.commit()
         
-        token = create_token({"email": found.email, "username": found.username})
-        
+        token = create_token({"id": str(found.user_id), "token_version": found.token_version})
         return token
 
         
@@ -64,5 +90,15 @@ def login(user: loginUser,db: Session = Depends(get_db)):
     
 
 
-# @router.get("/users")
-# def get_all_users(db: Session = Depends(get_db))
+@router.get('/users/me')
+def get_current_user(user = Depends(getUser)):
+   return "user"
+
+
+@router.get("/users/{id}")
+def get_user_by_id(id: UUID,db: Session = Depends(get_db)):
+   try:
+    found = db.query(User).get(id)
+    return found
+   except IntegrityError as err:
+       return err
